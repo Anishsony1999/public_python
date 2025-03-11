158b8e5cb4a0fa1023d85b9cc69e90ace60d74eb
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-03-11 09:00:03 +0530

    oops stared

--- OOPs.py
+++ OOPs.py
@@ -14,11 +14,68 @@
 # class ClassName:
 class Home:
     
-    def __init__(self):
-        pass
+    def __init__(self,name,color,fan,light):
+        self.name= name
+        self.light = light
+        self.color = color
+        self.fan = fan
 
+    def lights_on(self):
+        print(f"{self.name} {self.light} light will be on")
+
+    def hey_siri_fan_on(self):
+        print(f"{self.name} {self.fan} fan Will on")
 
 # string home1 = new String() - .net
-
 # obj = Class()
-gopika_home = Home()
+
+gopika_home = Home("gopika illam","White","Usha","White") 
+anish_home = Home("Sony Home","Black","Orion","Warm")
+
+anish_home.hey_siri_fan_on()
+anish_home.lights_on()
+gopika_home.lights_on()
+gopika_home.hey_siri_fan_on()
+
+
+print(anish_home.color)
+
+class Parent:
+
+    def __init__(self):
+        self.name = "Gopika"
+    
+    def add(self,x,y):
+        return x+y 
+    
+    def mult(self,x,y):
+        return x*y
+    
+class Child(Parent):
+    pass
+
+child = Child()
+
+ans = child.add(2,3)
+print(ans)
+print(child.name)
+
+class Bank:
+
+    def __init__(self):
+        self.name = "gopike"
+        self.__balance = 35000
+        self.add = "TVC"
+    
+    def set_balace(self,balance):
+        self.__balance = balance
+        print(f"Blance will added {self.__balance}")
+
+    def get_balace(self):
+        print(f"Your Balance is : {self.__balance}")
+
+gopika = Bank()
+gopika.set_balace(300)
+# gopika.__balance = 300
+# print(gopika.balance)
+gopika.get_balace()
